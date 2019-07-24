baf77eca25b3be37753dd1426547eac0de741b8f
Remi Kristelijn <rkristelijn@gmail.com>
2019-07-24 11:49:28 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from "react";
 import "./App.scss";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Dashboard from "./Dashboard";
+import AccountsView from "./Views/Accounts/AccountsView";
+import ContactsView from "./Views/Contacts/ContactsView";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+          <li>
+            <Link to="/accounts">Accounts</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/contacts" component={ContactsView} />
+        <Route exact path="/accounts" component={AccountsView} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
